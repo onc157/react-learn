ec3d9018c8e9cd1ab02e976dfd3b41261641840e
onc157 <onc157@yandex.ru>
2021-02-07 10:56:08 +0300

    init: project init

---
+++ way-of-the-samurai/first-project/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='header-wrapper'>
+      <img src='https://t3.ftcdn.net/jpg/02/32/74/26/360_F_232742698_HXvPlIXtt2FdshWwiuDyslaycN7COpPW.jpg' alt='car_logo' />
+    </header>
+  )
+}
+
+export default Header
---
+++ way-of-the-samurai/first-project/src/components/Profile.jsx
@@ -0,0 +1,29 @@
+const Profile = () => {
+  return (
+    <div className='content-wrapper'>
+      <img src='https://coresites-cdn-adm.imgix.net/whitelines_new/wp-content/uploads/2017/04/ValerianDucourtil_Mayrhofen_2Z7A2508_MattGEORGES-featured.jpg?fit=crop' alt='' />
+      <div className='content-user'>
+        <div className='content-user_logo'>
+          <img src='https://store.playstation.com/store/api/chihiro/00_09_000/container/UA/ru/999/EP2402-CUSA05624_00-AV00000000000110/1601168841000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000' alt='' />
+          description
+        </div>
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+          <div>
+            <div>
+              Post 1
+            </div>
+            <div>
+              Post 2
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
