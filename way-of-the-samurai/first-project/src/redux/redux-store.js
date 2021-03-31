ffce6c81b683c1a59c730ab55ae3c5fa8a96d3f8
onc157 <onc157@yandex.ru>
2021-03-31 21:19:48 +0300

    feat: added redux devtools

--- way-of-the-samurai/first-project/src/redux/redux-store.js
+++ way-of-the-samurai/first-project/src/redux/redux-store.js
@@ -13,7 +13,10 @@ const reducers = combineReducers({
   auth: authReducer,
 })
 
-const store = createStore(reducers)
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 window.store = store
 
