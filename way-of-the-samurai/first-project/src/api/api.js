fff08aae77f3f6abd93aedba84ee135567c84ab7
akulev <akulev@sweb,ru>
2021-06-07 10:19:09 +0300

    feat: added response error for auth

--- way-of-the-samurai/first-project/src/api/api.js
+++ way-of-the-samurai/first-project/src/api/api.js
@@ -1,59 +1,59 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
-  withCredentials: true,
-  headers: {
-    "API-KEY": "7761d243-5e49-41a9-bdf3-6f571311a764"
-  }
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        "API-KEY": "7761d243-5e49-41a9-bdf3-6f571311a764"
+    }
 })
 
 export const authAPI = {
-  me() {
-    return instance.get(`auth/me`)
-      .then(response => response.data)
-  },
+    me() {
+        return instance.get(`auth/me`)
+            .then(response => response.data)
+    },
 
-  login({email, password, rememberMe}) {
-    return instance.post(`auth/login`, {email, password, rememberMe})
-      .then(response => response.data)
-  },
+    login({email, password, rememberMe}) {
+        return instance.post(`auth/login`, {email, password, rememberMe})
+            .then(response => response.data)
+    },
 
-  logout() {
-    return instance.delete(`auth/login`)
-      .then(response => response.data)
-  }
+    logout() {
+        return instance.delete(`auth/login`)
+            .then(response => response.data)
+    }
 }
 
 export const usersAPI = {
-  getUsers(currentPage, pageSize) {
-    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
-      .then(response => response.data)
-  },
+    getUsers(currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data)
+    },
 
-  follow(userId) {
-    return instance.post(`follow/${userId}`)
-      .then(response => response.data)
-  },
+    follow(userId) {
+        return instance.post(`follow/${userId}`)
+            .then(response => response.data)
+    },
 
-  unfollow(userId) {
-    return instance.delete(`follow/${userId}`)
-      .then(response => response.data)
-  }
+    unfollow(userId) {
+        return instance.delete(`follow/${userId}`)
+            .then(response => response.data)
+    }
 }
 
 export const profileAPI = {
-  getProfile(userId) {
-    return instance.get(`profile/${userId}`)
-      .then(response => response.data)
-  },
+    getProfile(userId) {
+        return instance.get(`profile/${userId}`)
+            .then(response => response.data)
+    },
 
-  getStatus(userId) {
-    return instance.get(`profile/status/${userId}`)
-  },
+    getStatus(userId) {
+        return instance.get(`profile/status/${userId}`)
+    },
 
-  updateStatus(status) {
-    return instance.put('profile/status', {status})
-  }
+    updateStatus(status) {
+        return instance.put('profile/status', {status})
+    }
 }
 
--- way-of-the-samurai/first-project/src/redux/reducers/auth-reducer.js
+++ way-of-the-samurai/first-project/src/redux/reducers/auth-reducer.js
@@ -1,4 +1,4 @@
-import { authAPI } from '../../api/api';
+import {authAPI} from '../../api/api';
 
 const SET_AUTH_USER_DATA = 'SET_USER_DATA';
 // const USER_LOGIN = 'USER_LOGIN';
@@ -6,53 +6,57 @@ const SET_AUTH_USER_DATA = 'SET_USER_DATA';
 const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
 const initialState = {
-  id: null,
-  email: null,
-  login: null,
-  isAuth: false,
+    id: null,
+    email: null,
+    login: null,
+    isAuth: false,
 }
 
 const authReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case SET_AUTH_USER_DATA:
-      return { ...state, ...action.payload }
-    case TOGGLE_IS_FETCHING:
-      return { ...state, isFetching: action.isFetching }
-    default:
-      return state
-  }
+    switch (action.type) {
+        case SET_AUTH_USER_DATA:
+            return {...state, ...action.payload}
+        case TOGGLE_IS_FETCHING:
+            return {...state, isFetching: action.isFetching}
+        default:
+            return state
+    }
 }
 
-export const setAuthUserData = (id, email, login, isAuth) => ({ type: SET_AUTH_USER_DATA, payload: {
+export const setAuthUserData = (id, email, login, isAuth) => ({
+    type: SET_AUTH_USER_DATA, payload: {
         id, email, login, isAuth
-    } })
+    }
+})
 
 export const getAuthUserData = () => (dispatch) => {
-  authAPI.me()
-    .then(response => {
-      if (response.resultCode === 0) {
-        const { id, email, login } = response.data
-        dispatch(setAuthUserData(id, email, login, true))
-      }
-    })
+    authAPI.me()
+        .then(response => {
+            if (response.resultCode === 0) {
+                const {id, email, login} = response.data
+                dispatch(setAuthUserData(id, email, login, true))
+            }
+        })
 }
 
-export const userLogin = (authData) => (dispatch) => {
+export const userLogin = (authData, setStatus) => (dispatch) => {
     authAPI.login(authData)
         .then(response => {
-                if (response.resultCode === 0) {
-                    dispatch(getAuthUserData())
-          }
-    })
+            if (response.resultCode === 0) {
+                dispatch(getAuthUserData());
+            } else {
+                setStatus(response.messages);
+            }
+        })
 }
 
 export const userLogout = () => (dispatch) => {
-  authAPI.logout()
-    .then(response => {
-      if (response.resultCode === 0) {
-        dispatch(setAuthUserData(null, null, null, false))
-      }
-    })
+    authAPI.logout()
+        .then(response => {
+            if (response.resultCode === 0) {
+                dispatch(setAuthUserData(null, null, null, false))
+            }
+        })
 }
 
 export default authReducer
